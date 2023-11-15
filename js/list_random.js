a9b51d8d787a143b18532a99f9c7bbbf0f14c484
yacov binik <n0546003191@gmail.com>
2023-11-15 09:48:32 +0200

    Add game board, change style

--- js/list_random.js
+++ js/list_random.js
@@ -1,14 +1,14 @@
-  // Function to create and mix array
-  function createArray(number) {
-    let newNumber = number / 2;
-    let firstList = [];
-    for (let i = 1; i <= newNumber; i++) {
-      firstList.push(i);
-      firstList.push(i);
-    }
-    
-    return firstList;
+// Function to create and mix array
+function createArray(number) {
+  let newNumber = number / 2;
+  let firstList = [];
+  for (let i = 1; i <= newNumber; i++) {
+    firstList.push(i);
+    firstList.push(i);
   }
+
+  return firstList;
+}
 
 
 // Function to shuffle array הפונקציה מערבבת את המערך המוקרי בפונקציה הראשונה ומחיזרה אותו כבר מעורבב
@@ -24,10 +24,36 @@ function shuffleArray(array) {
   
   // Example usage
 
-  let arr = createArray(16);
-  let mixedArr = shuffleArray(arr) //פה הוא משנה את המשתנה המערך המסודר ופשוט מפעיל על המשתנה של המערך המסודר את העירבוב ועכשיו המערך = מעורבב
-  console.log(mixedArr);
-  
+let arr = createArray(50);
+let mixedArr = shuffleArray(arr) //פה הוא משנה את המשתנה המערך המסודר ופשוט מפעיל על המשתנה של המערך המסודר את העירבוב ועכשיו המערך = מעורבב
+console.log(mixedArr)
+function makeTheTable(mixedArr) {
+  let column  = 0;
+  if (mixedArr.length === 36) {
+    column = 6;
+  }
+  else if (mixedArr.length === 50) {
+    column = 10;
+  }
+  let column_counter = 0;
+  let tableHTML  = "<table><tr>";
 
 
+  for (let i of mixedArr) {
+    tableHTML += `<td class="hidden" name="${i}" style="width:150px; padding:4%;"> ${i} </td>`;
+    column_counter += 1;
+    if (column_counter === column) {
+      tableHTML += `</tr><tr>`;
+      column_counter = 0;
 
+    }
+
+  }
+  tableHTML += "</tr></table>";
+  document.getElementById("the_body_of_the_game").innerHTML = tableHTML;
+  document.getElementById("the_body_of_the_game").style.width = "fit-content";
+  document.getElementById("the_body_of_the_game").style.margin = "0 auto"; // זה יכניס את הטבלה באמצע של הדף
+
+
+}
+makeTheTable(mixedArr)
