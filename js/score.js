231da10d9208e030d85c8a62b90a9f7a161ded59
zvi gelbart <zmg80015@gmail.com>
2023-11-15 15:33:30 +0200

    add gameover

--- js/score.js
+++ js/score.js
@@ -1,23 +1,27 @@
-
-function reduceQueue(){
-    let queueNumber = document.getElementById("score");
-    let numberOfqueueNumber = parseInt(queueNumber.innerText)
-    let newQueue = numberOfqueueNumber - 1;
-    queueNumber.innerText = newQueue;
+function reduceRound() {
+  let RoundNumber = document.getElementById("score");
+  let numberOfRoundNumber = parseInt(RoundNumber.innerText);
+  if (numberOfRoundNumber > 0) {
+    let newRound = numberOfRoundNumber - 1;
+    RoundNumber.innerText = newRound;
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function compareCards(cardsEqual){ //הפונקציה מעדכנת את הנקודות והיא מקבלת את הפונקציה שבודקת האם הקלפים שווים אם אמת מוסיפה אם שקר מורידה
-    if (cardsEqual){
-        let numPoint = document.getElementById("point");
-        let parsPoint = parseInt(numPoint.innerText);
-        let newPoint = parsPoint + 100;
-        numPoint.innerText = newPoint;
-    }else{
-        let numPoint = document.getElementById("point");
-        let parsPoint = parseInt(numPoint.innerText);
-        let newPoint = parsPoint - 10;
-        numPoint.innerText = newPoint;
-    }
+function compareCards(cardsEqual) {
+  //הפונקציה מעדכנת את הנקודות והיא מקבלת את הפונקציה שבודקת האם הקלפים שווים אם אמת מוסיפה אם שקר מורידה
+  if (cardsEqual) {
+    let numPoint = document.getElementById("point");
+    let parsPoint = parseInt(numPoint.innerText);
+    let newPoint = parsPoint + 100;
+    numPoint.innerText = newPoint;
+    numberOfRoundNumber = +1
+  } else {
+    let numPoint = document.getElementById("point");
+    let parsPoint = parseInt(numPoint.innerText);
+    let newPoint = parsPoint - 10;
+    numPoint.innerText = newPoint;
+  }
 }
-
-
