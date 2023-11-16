e5c4b13150b88d3d7efe69bb7c96d8a7d4fc5367
zvi gelbart <zmg80015@gmail.com>
2023-11-16 10:01:44 +0200

    add win

--- js/gameOver.js
+++ js/gameOver.js
@@ -1,6 +1,6 @@
 function endGame(time,point,level){
     let document
     if (time === 0 | point=== 0 | level === 0){
-        return //פןנקציה שמעלה את סןף את המשחק
+        return // בדייב פןנקציה שמעלה את סןף את המשחק
     }
 }
